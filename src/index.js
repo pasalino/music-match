e7460588c824495f7af7cd20409046982349c538
Pasqualino de Simone <pasalino@MBPdiPasqualino.station>
2017-03-19 16:57:26 +0100

    Refactor index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import React from 'react';
 
-ReactDOM.render( <
-    App / > , document.getElementById("root")
+ReactDOM.render(<App />, document.getElementById("root")
 );
